166854ecc5cf99cfaf67be6d05302aef7c4077a9
Valeri Nistor <nistor.valeri@gmail.com>
2013-10-29 21:31:04 +0200

    added rules

--- config.lua
+++ config.lua
@@ -514,9 +514,12 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    { rule = { class = "Google-chrome" },
+	  properties = { tag = tags[1][2] } },
+    { rule = { class = "Eclipse" },
+	  properties = { tag = tags[1][1] } },
+    { rule = { class = "Skype" },
+	  properties = { tag = tags[1][4] } }
 }
 -- }}}
 
