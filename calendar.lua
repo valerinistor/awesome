a4639dc1b8faec5a0c067a78335958f146d6513e
Valeri Nistor <nistor.valeri@gmail.com>
2016-03-10 02:19:31 +0200

    calendar:fix month switch

--- calendar.lua
+++ calendar.lua
@@ -3,24 +3,38 @@
 -- and with the following changes:
 --   + transformed to module
 --   + the current day formating is customizable
+-- modified by Jörg Thalheim (Mic92) <jthalheim@gmail.com> (2011), under the same licence,
+-- and with the following changes:
+--   + use tooltip instead of naughty.notify
+--   + rename it to calendar
+--   + lua52 compliant module
+--
+-- 1. require it in your rc.lua
+--  require("calendar")
+-- 2. attach the calendar to a widget of your choice (ex mytextclock)
+--  calendar.register(mytextclock)
+--    If you don't like the default current day formating you can change it as following
+--  calendar.register(mytextclock, "<b>%s</b>") -- now the current day is bold instead of underlined
+--
+-- # How to Use #
+-- Just hover with your mouse over the widget, you register and the calendar popup.
+-- On clicking or by using the mouse wheel the displayed month changes.
+-- Pressing Shift + Mouse click change the year.
 
-local string = string
-local tostring = tostring
-local os = os
-local capi = {
-    mouse = mouse,
-    screen = screen
-}
+-- Orginal: https://github.com/Mic92/utils/blob/master/cal.lua
+
 local awful = require("awful")
-local wibox = require("wibox")
 local naughty = require("naughty")
-module("calendar")
+
+local string = {format = string.format}
+local os = {date = os.date, time = os.time}
+local mouse = mouse
 
 local calendar = {}
+local state = {}
 local current_day_format = "<u>%s</u>"
 
 function displayMonth(month, year, weekStart)
-
     local t, wkSt = os.time{year = year, month = month + 1, day = 0}, weekStart or 1
     local d = os.date("*t", t)
     local mthDays, stDay = d.day, (d.wday - d.day - wkSt + 1) % 7
@@ -49,58 +63,69 @@ function displayMonth(month, year, weekStart)
         if os.date("%Y-%m-%d") == os.date("%Y-%m-%d", t) then
             x = string.format(current_day_format, d)
         end
-        if (#(tostring(d)) == 1) then
+        if d < 10 then
             x = " " .. x
         end
         lines = lines .. "  " .. x
         writeLine = writeLine + 1
+    end
+    if stDay + mthDays < 36 then
+        lines = lines .. "\n"
+    end
+    if stDay + mthDays < 29 then
+        lines = lines .. "\n"
     end
     local header = os.date("%B %Y\n", os.time{year = year, month = month, day = 1})
 
     return header .. "\n" .. lines
 end
 
-function switchNaughtyMonth(switchMonths)
-    if (#calendar < 3) then return end
-    local swMonths = switchMonths or 1
-    calendar[1] = calendar[1] + swMonths
-    if calendar[3].box.widgets == nil then calendar[3].box.wigets = wibox.widget.textbox end
-    calendar[3].box.widgets[2].text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(calendar[1], calendar[2], 2))
-end
 
-function switchNaughtyGoToToday()
-    if (#calendar < 3) then return end
-    local swMonths = switchMonths or 1
-    calendar[1] = os.date("*t").month
-    calendar[2] = os.date("*t").year
-    switchNaughtyMonth(0)
-end
-
-function addCalendarToWidget(mywidget, custom_current_day_format)
+function calendar.register(calendarWidget, custom_current_day_format)
     if custom_current_day_format then current_day_format = custom_current_day_format end
 
-    mywidget:connect_signal("mouse::enter", function ()
+    calendarWidget:connect_signal("mouse::enter", function ()
             local month, year = os.date('%m'), os.date('%Y')
-            calendar = {month, year,
-                naughty.notify({
-                        text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(month, year, 2)),
-                        timeout = 0,
-                        hover_timeout = 0.5,
-                        screen = capi.mouse.screen
-                    })
-            }
+            state = {month, year}
+            naughty.notify({
+                    text = string.format('<span font_desc="monospace">%s</span>', displayMonth(month, year, 2)),
+                    timeout = 0,
+                    hover_timeout = 0.5,
+                    screen = mouse.screen
+                })
         end)
-    mywidget:connect_signal('mouse::leave', function () naughty.destroy(calendar[3]) end)
 
-    mywidget:buttons(awful.util.table.join(
-            awful.button({}, 1, function () switchNaughtyMonth(-1) end),
-            awful.button({}, 2, switchNaughtyGoToToday),
-            awful.button({}, 3, function () switchNaughtyMonth(1) end),
-            awful.button({}, 4, function () switchNaughtyMonth(-1) end),
-            awful.button({}, 5, function () switchNaughtyMonth(1) end),
-            awful.button({'Shift'}, 1, function () switchNaughtyMonth(-12) end),
-            awful.button({'Shift'}, 3, function () switchNaughtyMonth(12) end),
-            awful.button({'Shift'}, 4, function () switchNaughtyMonth(-12) end),
-            awful.button({'Shift'}, 5, function () switchNaughtyMonth(12) end)
-        ))
+    calendarWidget:connect_signal('mouse::leave', function () naughty.destroy(calendar[3]) end)
+
+    calendarWidget:buttons(awful.util.table.join(
+            awful.button({}, 1, function () switchMonth(-1) end),
+            awful.button({}, 2, function () goToToday() end),
+            awful.button({}, 3, function () switchMonth(1) end),
+            awful.button({}, 4, function () switchMonth(-1) end),
+            awful.button({}, 5, function () switchMonth(1) end),
+            awful.button({'Shift'}, 1, function () switchMonth(-12) end),
+            awful.button({'Shift'}, 3, function () switchMonth(12) end),
+            awful.button({'Shift'}, 4, function () switchMonth(-12) end),
+            awful.button({'Shift'}, 5, function () switchMonth(12) end)))
 end
+
+
+function switchMonth(delta)
+    state[1] = state[1] + delta
+
+    naughty.notify({
+            text = string.format('<span font_desc="monospace">%s</span>', displayMonth(state[1], state[2], 2)),
+            timeout = 0,
+            hover_timeout = 0.5,
+            screen = mouse.screen
+        })
+end
+
+
+function goToToday()
+    state[1] = os.date('%m')
+    state[2] = os.date('%Y')
+    switchMonth(0)
+end
+
+return calendar
